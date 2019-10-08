e2cc6e3621e4cca6e6e6f6ba12c469b40f3c87e3
jirm <jirm1893@gmail.com>
2019-10-08 19:16:58 +0800

    $ls_date

--- jirm.bat
+++ jirm.bat
@@ -1,26 +1,7 @@
-ls_date=`date +%Y%m%d`
-git-pull() {
-    echo git fetch --all && git reset --hard origin/master && git pull ...
-    git fetch --all && git reset --hard origin/master && git pull
-    echo  The pull end!
-}
-git-push() {
-    echo git add . && git commit -m $ls_date && git push origin master ...
-    git add . && git commit -m $ls_date && git push origin master
-    echo  The push end!
-}
-case "$1" in
-        git-pull)
-          git-pull
-           ;;
-
-        git-push)
-          git-push
-           ;;
-
-           *)
-          echo "usage git-pull|git-push"
-           ;;
-
-esac
+set var1=%date%;
+if "%1"=="git-pull" (echo git fetch --all && git reset --hard origin/master && git pull ... 
+  git fetch --all && git reset --hard origin/master && git pull 
+  echo  The pull end!) else if "%1"=="git-push" ( echo git add . && git commit -m $ls_date && git push origin master ...
+  git add . && git commit -m $ls_date && git push origin master
+  echo  The push end!) else ( echo "usage git-pull|git-push" )
 
