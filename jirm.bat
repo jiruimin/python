1f6b127a92d15b5c74596721d01869324e0d4138
jirm <jirm1893@gmail.com>
2019-10-08 19:18:53 +0800

    $ls_date

--- jirm.bat
+++ jirm.bat
@@ -1,5 +1,11 @@
 set var1=%date%;
-if "%1"=="git-pull" ( git fetch --all && git reset --hard origin/master && git pull 
-  echo  The pull end!) else if "%1"=="git-push" ( git add . && git commit -m $ls_date && git push origin master
-  echo  The push end!) else ( echo "usage git-pull|git-push" )
+if "%1"=="git-pull" ( 
+  git fetch --all && git reset --hard origin/master && git pull 
+  echo  The pull end!
+) else if "%1"=="git-push" (
+  git add . && git commit -m $ls_date && git push origin master
+  echo  The push end!
+) else (
+  echo "usage git-pull|git-push"
+)
 
