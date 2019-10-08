810d90b242b2436149d26698ea54d0a17da68e30
jirm <jirm1893@gmail.com>
2019-10-08 19:27:13 +0800

    2019/10/08 周二

--- jirm.bat
+++ jirm.bat
@@ -1,11 +1,11 @@
 REM @echo off
-set var1=%date%
+set date=%date%
 set op=%1
 if "%op%"=="git-pull" ( 
   git fetch --all && git reset --hard origin/master && git pull 
   echo  The pull end!
 ) else if "%op%"=="git-push" (
-  git add . && git commit -m $var1 && git push origin master
+  git add . && git commit -m "%date%" && git push origin master
   echo  The push end!
 ) else (
   echo "usage git-pull|git-push"
